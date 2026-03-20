96c256a6cfe439e9d81584003b991a478a57a6c1
Thamaruj <thamarujalthotage1@gmail.com>
2026-03-20 10:58:45 +0530

    Changed font to "Manrope"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/Navbar"; // 1. Import your new Navbar component
+import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
 
-const inter = Inter({ subsets: ["latin"] });
+const manrope = Manrope({ 
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"]
+});
 
 export const metadata: Metadata = {
   title: "Rezenait",
@@ -13,22 +16,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;  
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen flex flex-col bg-dot-pattern`}>
-        
+      <body className={`${manrope.className} min-h-screen flex flex-col bg-dot-pattern`}>
         <Navbar />
-
-        {/* We wrap children in a 'flex-grow' main tag so the footer is always pushed to the bottom of the screen, even on short pages! */}
         <main className="flex-grow">
           {children}
         </main>
-
         <Footer />
-        
       </body>
     </html>
   );
